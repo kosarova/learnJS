d34879a5bd120e028c470b7a5e04cc93f7ce3e78
Kobzarev Roman <krv2306@gmail.com>
2021-01-21 17:10:35 +0300

    change obj-calc with constructor

--- calc_obj_new/calc_obj.js
+++ calc_obj_new/calc_obj.js
@@ -1,15 +1,17 @@
-let calculator = {
-    read () {
-        this.a = +prompt("a?");
-        this.b = +prompt("b?")
-    },
-    sum () {
-        return this.a + this.b
-    },   
-    mul () {
-        return this.a * this.b
-    }
-};
-calculator.read();
-alert( calculator.sum() )
-alert( calculator.mul() )
+function Calc() {
+    this.read = function(){
+        this.a = +prompt('first num', '');
+        this.b = +prompt('second num', '');
+    };
+    this.mul = function() {
+        return this.a * this.b;
+    };
+    this.sum = function() {
+        return this.a + this.b;
+    };
+}
+
+let calc = new Calc();
+calc.read();
+alert('Sum=' + calc.sum() );
+alert('Mul=' + calc.mul() );
